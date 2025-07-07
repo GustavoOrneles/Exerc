a40e6949d4314e86a84b57040d153c1a605f33ab
GustavoOrneles <malwareby427@gmail.com>
2025-07-07 19:32:58 -0300

    Imagem de fundo e fontes

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-
-
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
     return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'))
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('.src/styles/*.scss', gulp.parallel(styles))
+// Watcher correto
+function watch() {
+    gulp.watch('./src/styles/**/*.scss', styles);
+    gulp.watch('./src/images/**/*', images);
 }
+
+// Executa styles e images inicialmente e mantém assistindo para sempre
+exports.default = gulp.series(
+    gulp.parallel(styles, images),
+    watch
+);
+exports.watch = watch;
